610b88a4e386f0de6adb2dc8cec6a704584578ef
Jonathan Holifield <jonholifield@gmail.com>
2016-09-24 18:40:04 -0400

    First Commit
    Start of a deep q learner

---
+++ deepq.py
@@ -0,0 +1,24 @@
+"""
+Solve OpenAI Gym Cartpole V1 with DQN.
+"""
+import gym
+
+import numpy as np
+import tensorflow as tf
+
+
+if __name__ == '__main__':
+    env = gym.make('CartPole-v1')
+    env.monitor.start('training_dir', force=True)
+
+    for i_episode in range(20):
+        observation = env.reset()
+        for t in range(100):
+            env.render()
+            print(observation)
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            if done:
+                print("Episode finished after {} timesteps".format(t+1))
+                break
+    env.monitor.close()
